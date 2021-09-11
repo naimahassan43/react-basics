fa80f8d6a595f141c2ba142e334abf9a3ccb552b
Naima Hassan <naima.hassan434@gmail.com>
2021-09-11 22:33:32 +0100

    Initialize from React State

---
+++ src/components/Clock.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.clockTimer = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.clockTimer);
+  }
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <h1 className="heading">
+        <span className="text">
+          It is {this.state.date.toLocaleTimeString(this.props.locale)}
+        </span>
+      </h1>
+    );
+  }
+}
+
+export default Clock;
